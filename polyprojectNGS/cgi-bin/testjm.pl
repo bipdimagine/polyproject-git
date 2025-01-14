1c583de19b906bcde8f18dbe9a0b0fd346f1d229
plaza <plaza@sanger.cm.cluster>
2025-01-14 11:37:30 +0100

    test

---
+++ polyprojectNGS/cgi-bin/testjm.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+########################################################################
+###### manageRunPlt.pl
+########################################################################
+#use CGI;
+use CGI qw/:standard :html3/;
+use strict;
+use FindBin qw($Bin);
+use lib "$Bin/GenBo";
+use lib "$Bin/GenBo/lib/GenBoDB";
+use lib "$Bin/GenBo/lib/obj-nodb";
+use lib "$Bin/GenBo/lib/GenBoDB/writeDB";
+use lib "$Bin/GenBo/script/ngs_exome/last_script/packages";
+use lib "$Bin/packages"; 
+
+use lib "$Bin/../../polymorphism-cgi/packages/export";
