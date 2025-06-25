bb267576c993669ea6dbd5c1e71f3c849a54b4ef
plaza <plaza@sanger.cm.cluster>
2025-06-25 10:46:36 +0200

    export

--- polyprojectNGS/cgi-bin/stat.pl
+++ polyprojectNGS/cgi-bin/stat.pl
@@ -13,6 +13,7 @@ use lib "$Bin/GenBo/lib/GenBoDB/writeDB";
 use lib "$Bin/GenBo/script/ngs_exome/last_script/packages";
 use lib "$Bin/packages"; 
 use lib "$Bin/../../polymorphism-cgi/packages/export";
+use lib "$Bin/GenBo/../polymorphism-cgi/packages/export";
 
 use GenBoPatient;
 #use util_file qw(readXmlVariations);
