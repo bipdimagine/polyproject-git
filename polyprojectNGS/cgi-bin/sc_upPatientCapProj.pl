fb6ffaa5424ec86b0b0d48da1a9ec2f455e17623
plaza <plaza@sanger.cm.cluster>
2026-04-02 15:01:31 +0200

    optional: with patients

--- polyprojectNGS/cgi-bin/sc_upPatientCapProj.pl
+++ polyprojectNGS/cgi-bin/sc_upPatientCapProj.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 ########################################################################
 ###### sc_newCapHG38.pl #################################################
-#./sc_upPatientCapProj.pl -project=NGS2025_08677 -capture=Twist_plus
+#./sc_upPatientCapProj.pl -project=NGS2025_08677 -capture=Twist_plus -patient="PLU-D_M","PLU-D_F"
 ########################################################################
 #use CGI qw/:standard :html3/;
 use strict;
@@ -34,12 +34,17 @@ my $help;
 my $insert;
 my $project; #
 my $capture; #
+my $patient; #
 
 my $message ="Usage :
 	$0	-h or -help 
   	$0	-project=<NGS-Project>			# Input NGS Project Name 
-   	$0	-capture=<Capture_Name>			# Input Exon Capture Name 
-  	$0		 -insert                        # to update
+   	$0	-capture=<Capture_Name>			# Input Exon Capture Name
+   	$0	-insert                         # to update
+   	
+   	Optional: with Patients
+  	$0	-project=<NGS-Project> -capture=<Capture_Name> -patient=<patientName_1>,...,<patientName_N>	[-insert]	# Patient Name List 	
+  	$0	-project=<NGS-Project> -capture=<Capture_Name>	[-insert] < File_In		# File of Patient Name
  \n";
 
 GetOptions(
@@ -48,7 +53,8 @@ GetOptions(
 	'insert'  => \$insert,
 	'project=s' => \$project,
 	'capture=s' => \$capture,
-);# or confess($message);
+	'patient=s' => \$patient,
+);
 
 if ($h|$help) {
 	confess ($message);	
@@ -60,33 +66,77 @@ my $res = queryPolyproject::getProjectFromName($buffer->dbh,$project);
 die("$message Error: Unknown Project : $project\n") unless $res->{projectId};
 
 my $capInfo= queryPolyproject::getCaptureFromName($buffer->dbh,$capture);
-#warn Dumper $capInfo;
 die("$message Error: Unknown Capture : $capture\n") unless $capInfo->{capName};
-#warn Dumper $capInfo->{captureId};
 print "#############################################################################################\n";
 print "Project: $res->{projectId} $project - Capture: $capInfo->{captureId} $capInfo->{capName}\n";
 print "#############################################################################################\n\n";
 
 
-my $cpt=1;
+my @InPatient;
+if (!-t STDIN) {
+	# File , not Terminal
+	while (my $line = <>) {
+    	chomp $line;
+   	 	next unless $line;
+ 		my $patproj = getPatientProject_byNamePatId($buffer->dbh,$line,$res->{projectId});  
+		die("$message Error: In Project $project, Unknown Patient $line ....\n") unless $patproj->{patient_id};
+		my %si;
+		$si{patient_id} = $patproj->{patient_id};
+		$si{name} = $patproj->{name};
+		$si{project_id} = $patproj->{project_id};
+		push(@InPatient,\%si);
+	}
+}	
+
+if ($patient) {
+	my @patient=split(/,/,$patient);
+	foreach my $m (@patient){
+		my $patproj = getPatientProject_byNamePatId($buffer->dbh,$m,$res->{projectId}); 
+		die("$message Error: In Project $project, Unknown Patient $m ....\n") unless $patproj->{patient_id};
+		my %si;
+		$si{patient_id} = $patproj->{patient_id};
+		$si{name} = $patproj->{name};
+		$si{project_id} = $patproj->{project_id};
+		push(@InPatient,\%si);
+	}			
+}
+
+my @Patlist;
 my $patientList = queryPolyproject::getPatientProjectInfo($buffer->dbh,$res->{projectId});
+
+if (scalar(@InPatient)>0) {
+	@Patlist= @InPatient;
+} else {
+	@Patlist=@$patientList;
+}
 
 ### Autocommit dbh ###########
 my $dbh = $buffer->dbh;
 $dbh->{AutoCommit} = 0;
 #############################
-
-
-
-foreach my $m (@$patientList) {
+my $cpt=1;
+foreach my $m (@Patlist) {
 	print "$cpt\tPatient $m->{patient_id} $m->{name}\tProject: $m->{project_id} $project\n" unless $insert;
 	queryPolyproject::upPatientCaptureOnly($buffer->dbh,$m->{patient_id},$capInfo->{captureId})  if $insert;
 	print "$cpt\tPatient $m->{patient_id} $m->{name}\tProject: $m->{project_id} $project\t==> Capture: $capInfo->{captureId} $capInfo->{capName} Done...\n" if $insert;
 	$cpt++;
 }
 print "Add option -insert to update \n" unless $insert;
-
 $dbh->commit();	
+
+sub getPatientProject_byNamePatId {
+	my ($dbh,$patname,$pid) = @_;
+	my $query = qq{
+		select a.patient_id,a.name,a.project_id
+		FROM PolyprojectNGS.patient a
+		where a.name='$patname'
+		and a.project_id='$pid';
+	};
+	my $sth = $dbh->prepare($query);
+	$sth->execute();
+	my $s = $sth->fetchrow_hashref();
+	return $s;
+}
 
 exit(0);
 
